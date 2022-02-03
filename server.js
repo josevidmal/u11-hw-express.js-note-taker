44c5dcd126250aaf7b433c426c53731baea4aca5
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-02-02 22:02:13 -0600

    Adding readFromFile, writeToFile and readAndAppend functions using the fs module for data persistency. Calling readFromFile function in get /api/notes route. Calling readAndAppend function in post /api/notes route. New notes added to db.json file while doing tests to check correct functionality of the application

--- server.js
+++ server.js
@@ -21,9 +21,28 @@ app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) => 
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => 
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
 app.get("/api/notes", (req, res) => {
     console.info(`${req.method} request received to get notes`)
-    return res.json(db);
+    readFromFile("./db/db.json").then((data) => res.status(200).json(JSON.parse(data)));
 });
 
 app.post("/api/notes", (req, res) => {
@@ -36,8 +55,10 @@ app.post("/api/notes", (req, res) => {
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuid(),
         };
+
+        readAndAppend(newNote, "./db/db.json");
 
         const response = {
             status: "success",
