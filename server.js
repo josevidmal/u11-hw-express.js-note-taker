47a9ed612b22dc252567d3d2a12fea0015e5387e
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-02-03 21:51:49 -0600

    Adding DELETE /api/notes/:id route to read db.json file, find a specific note id, remove it and after that write all the remaining notes again. Added new set of notes in db.json file while testing

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const util = require("util");
 const express = require("express");
 const path = require("path");
-const db = require("./db/db.json");
 const uuid = require("./helpers/uuid");
+let db = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -66,22 +66,31 @@ app.post("/api/notes", (req, res) => {
         };
 
         console.log(response);
-        res.status(201).json(response);
+        return res.status(201).json(response);
     } else {
-        res.status(500).json("Error in posting note");
+        return res.status(500).json("Error in posting note");
     }
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-    const deleteNote = req.params.id.toLowerCase();
+    console.info(`${req.method} request received to remove a note`);
+    
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        console.log(data);
 
-    for (let i = 0; i < db.length; i++) {
-        if (deleteNote === db[i].id.toLowerCase()) {
-            return res.json(db[i]);
+        const id = req.params.id;
+        const parsedNotes = JSON.parse(data);
+        const toDelete = parsedNotes.find(note => note.id === id);
+    
+        if (toDelete) {
+            data = parsedNotes.filter(note => note.id !== id);
+            writeToFile("./db/db.json", data);
+            return res.status(200).json(toDelete);
+        } else {
+            return res.status(404).json("There doesn't exist a Note with such id");
         }
-    }
-    return res.json("There doesn't exist a Note with such id");
-})
+    })
+}); 
 
 app.get("*", (req, res) => 
     res.sendFile(path.join(__dirname, "public/index.html"))
